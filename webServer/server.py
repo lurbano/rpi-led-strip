33ed8a461b08658b37022ce36173f4067357e0ee
lurbano <lurbano@earthsciweb.org>
2021-02-12 11:44:48 -0600

    small change

--- webServer/server.py
+++ webServer/server.py
@@ -76,7 +76,7 @@ class WSHandler(tornado.websocket.WebSocketHandler):
 				for i in range(nPix):
 					pixels[i] = (0,0,0)
 				pixels.show()
-				self.write_message{"info":"cleared"}
+				
 
 
             # if msg["what"] == "dist":
