8d4c59cce342ee0c70fbaabf731724a57ab0e8c4
lurbano <lurbano@earthsciweb.org>
2021-02-12 23:14:08 -0600

    test

--- webServer/ledPixels.py
+++ webServer/ledPixels.py
@@ -57,8 +57,9 @@ class ledPixels:
         print(f'brightness: {self.brightness}')
         for i in range(self.nPix):
             c = self.oldColors[i]
-            col = (c[0]*b, c[1]*b, c[2]*b)
+            col = (int(c[0]*b), int(c[1]*b), int(c[2]*b)
             self.pixels[i] = col
+        print(self.pixels[-1])
         self.pixels.show()
 
     def blue(self):
