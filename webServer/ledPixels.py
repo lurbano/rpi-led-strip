0a7e6e0c582cc9000a8a4c6cce78d8e9d36bb1c6
lurbano <lurbano@earthsciweb.org>
2021-02-12 22:10:06 -0600

    test

--- webServer/ledPixels.py
+++ webServer/ledPixels.py
@@ -5,6 +5,7 @@ import time
 def hex_to_rgb(value):
     value = value.lstrip('#')
     lv = len(value)
+    print("hex")
     return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
 
 def rgb_to_hex(rgb):
