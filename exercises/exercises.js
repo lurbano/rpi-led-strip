967d529ca26ce307e92615c43d2ffcabc22a5e55
lurbano <lurbano@earthsciweb.org>
2021-04-11 20:29:05 -0500

    Styling

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -2,8 +2,12 @@ $(".toggle-next").click(function(){
   $(this).next().toggle();
   if ($(this).next().is(":visible")){
     $(this).parent().css("border", "1px solid red");
+    let txt = $(this).html().replace("Show", "Hide").replace("▼", "▲");
+    $(this).html(txt);
   }
   else {
     $(this).parent().css("border", "none");
+    let txt = $(this).html().replace("Hide", "Show").replace("▲", "▼");
+    $(this).html(txt);
   }
 })
