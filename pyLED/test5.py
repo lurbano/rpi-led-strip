139956499b4746b8ff96f61e81090eeedc83785c
lurbano <lurbano@earthsciweb.org>
2021-04-26 20:08:18 -0500

    functions

---
+++ pyLED/test5.py
@@ -0,0 +1,16 @@
+import board
+import neopixel
+import time
+
+nPix = 20
+pixels = neopixel.NeoPixel(board.D18, nPix)
+
+def turnOff(n):
+	for i in range(n):
+		pixels[i] = (0,0,0)
+
+for i in range(0, nPix, 2):
+	pixels[i] = (0, 0, 255)
+
+time.sleep(2)
+turnOff(nPix)
