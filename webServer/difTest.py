e53ca06a76f96d46d4f2b3bc96d4b396ffbac0fc
lurbano <lurbano@earthsciweb.org>
2021-05-13 12:12:23 -0500

    play

--- webServer/difTest.py
+++ webServer/difTest.py
@@ -15,6 +15,7 @@ for i in range(nPix):
 ledPix = ledPixels(nPix, board.D18)
 
 ledPix.pixels[5] = (0, 255, 0)
+ledPix.pixels[10] = (255, 0, 0)
 ledPix.pixels[15] = (0, 0, 255)
 ledPix.pixels.show()
 
