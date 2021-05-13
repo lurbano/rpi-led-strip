8429d5f1dd5550764053fd76ded7f7a68b9423aa
lurbano <lurbano@earthsciweb.org>
2021-05-13 08:56:58 -0500

    test

--- webServer/difTest.py
+++ webServer/difTest.py
@@ -9,6 +9,6 @@ ledPix = ledPixels(20, board.D18)
 ledPix.pixels[3] = (255, 0, 0)
 ledPix.pixels.show()
 
-for i in range(10):
+for i in range(100):
     ledPix.diffuse()
-    time.sleep(0.5)
+    time.sleep(0.1)
