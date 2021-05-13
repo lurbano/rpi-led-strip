2b26211c7453e28783886ee6cab385f3a70922fb
lurbano <lurbano@earthsciweb.org>
2021-05-13 09:00:40 -0500

    test

--- webServer/difTest.py
+++ webServer/difTest.py
@@ -1,14 +1,25 @@
 import board
 import neopixel
 import time
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 from ledPixels import *
 
-ledPix = ledPixels(20, board.D18)
+nPix = 20
+r = []
+for i in range(nPix):
+    r.append(0)
+
+ledPix = ledPixels(nPix, board.D18)
 
 ledPix.pixels[3] = (255, 0, 0)
 ledPix.pixels.show()
 
 for i in range(100):
     ledPix.diffuse()
+    for j in range(nPix):
+        r[j] = round(ledPix.pixels[j][0])
+    print(r)
     time.sleep(0.1)
