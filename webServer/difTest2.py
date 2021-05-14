50f5954b52b6e4037d9b2eca60af6bdf76f9e2c6
lurbano <lurbano@earthsciweb.org>
2021-05-14 11:47:12 -0500

    diffusion test: marching pixel

---
+++ webServer/difTest2.py
@@ -0,0 +1,23 @@
+import board
+import neopixel
+import time
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+
+from ledPixels import *
+
+nPix = 20
+
+ledPix = ledPixels(nPix, board.D18)
+
+# ledPix.pixels[5] = (0, 255, 0)
+# ledPix.pixels[10] = (255, 0, 0)
+# ledPix.pixels[15] = (0, 0, 255)
+# ledPix.pixels.show()
+#
+# ledPix.diffuse(k=0.1, nsteps=100)
+
+for i in range(nPix):
+    ledPix.pixels[i] = (0,255,0)
+    ledPix.diffuse(nsteps=10)
