2a9570fcc3553329da614911bdf57d92712f949e
lurbano <lurbano@earthsciweb.org>
2021-05-14 21:49:24 -0500

    test

--- webServer/normTest.py
+++ webServer/normTest.py
@@ -6,6 +6,6 @@ nPix = 43
 
 ledPix = ledPixels(nPix, board.D18)
 
-for i in range(nPix):
-    ledPix.normalDistribution(n=i, sig=2)
+for i in range(nPix*2):
+    ledPix.normalDistribution(n=float(i)/2.0, sig=2)
     time.sleep(0.1)
