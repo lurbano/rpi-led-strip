56e5ee0ec0eb5c88580bc0ffbdc11e55d13ac459
lurbano <lurbano@earthsciweb.org>
2021-05-14 22:01:22 -0500

    test

--- webServer/normTest.py
+++ webServer/normTest.py
@@ -10,4 +10,4 @@ for i in range(nPix*3):
     ledPix.resetPix()
     ledPix.normalDistribution(n=float(i)/3.0, sig=2, col=(255, 255,0))
     ledPix.pixels.show()
-    time.sleep(0.1)
+    time.sleep(0.1/3)
