16ee2461b11ce3a579c8a09857202c103499c702
lurbano <lurbano@earthsciweb.org>
2021-05-14 22:02:27 -0500

    fix

--- webServer/normTest.py
+++ webServer/normTest.py
@@ -3,11 +3,12 @@ import time
 from ledPixels import *
 
 nPix = 43
+speed = 4
 
 ledPix = ledPixels(nPix, board.D18)
 
-for i in range(nPix*3):
+for i in range(nPix*speed):
     ledPix.resetPix()
-    ledPix.normalDistribution(n=float(i)/3.0, sig=2, col=(255, 255,0))
+    ledPix.normalDistribution(n=float(i)/speed, sig=2, col=(255, 255,0))
     ledPix.pixels.show()
-    time.sleep(0.1/3)
+    time.sleep(0.1/speed)
