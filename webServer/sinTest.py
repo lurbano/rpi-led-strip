c106195b593d3f4bb904651e4c89e69ad8c58033
lurbano <lurbano@earthsciweb.org>
2021-05-17 15:23:00 -0500

    test

--- webServer/sinTest.py
+++ webServer/sinTest.py
@@ -8,8 +8,7 @@ speed = 4
 
 ledPix = ledPixels(nPix, board.D18)
 
-for phase in np.arange(0, 2*np.pi, 0.01):
-    ledPix.clear()
-    ledPix.sinFunc(2,phase,(0,0,100), 100)
-    ledPix.pixels.show()
-    time.sleep(0.01)
+#for phase in np.arange(0, 2*np.pi, 0.01):
+ledPix.sinFunc(2,phase,(0,0,100), 100)
+ledPix.pixels.show()
+time.sleep(0.01)
