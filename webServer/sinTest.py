f8a3e62072ffa76689474f55f17832dc85336f15
lurbano <lurbano@earthsciweb.org>
2021-05-17 14:38:36 -0500

    Sin function

---
+++ webServer/sinTest.py
@@ -0,0 +1,21 @@
+import time
+import numpy as np
+
+from ledPixels import *
+
+nPix = 24
+speed = 4
+
+ledPix = ledPixels(nPix, board.D18)
+
+for i in range(nPix):
+    r = np.sin(np.pi*i/nPix)
+    ledPix.pixels[i] = (r,0,0)
+ledPix.show()
+
+# for i in range(nPix*speed):
+#     ledPix.resetPix()
+#     ledPix.normalDistribution(n=float(i)/speed, sig=2, col=(255, 0,0))
+#     ledPix.normalDistribution(n=nPix-float(i)/speed, sig=2, col=(0, 255,0))
+#     ledPix.pixels.show()
+#     time.sleep(0.1/speed)
