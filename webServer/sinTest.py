5e0b5a28dac1794c752309ce3cc56a65cc469d86
lurbano <lurbano@earthsciweb.org>
2021-05-17 14:47:30 -0500

    test

--- webServer/sinTest.py
+++ webServer/sinTest.py
@@ -9,10 +9,12 @@ speed = 4
 ledPix = ledPixels(nPix, board.D18)
 
 for i in range(nPix):
-    r = 100 * np.sin(2*np.pi*i/nPix)
+    r = 100 * np.sin((2*np.pi*i/nPix)+np.pi)
+    g = 100 * np.sin((2*np.pi*i/nPix))
     r = max(r, 0.0)
+    g = max(g, 0.0)
     print(r)
-    ledPix.pixels[i] = (0,r, 0)
+    ledPix.pixels[i] = (r,g, 0)
     print(ledPix.pixels[i])
 ledPix.pixels.show()
 
