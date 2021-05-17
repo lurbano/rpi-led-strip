ddb3319dfade67d898efd1237bc45ccd3f3aa16d
lurbano <lurbano@earthsciweb.org>
2021-05-17 17:52:21 -0500

    cleanup

--- webServer/sinTest.py
+++ webServer/sinTest.py
@@ -11,14 +11,13 @@ speed = 4
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--freq", default=1, type=float, help = "Frequency")
 parser.add_argument("-p", "--phase", default=0, type=float, help = "Phase")
-parser.add_argument("-c", "--color", default="(0,20,0)", type=str, help = "Color")
+parser.add_argument("-c", "--color", default="(0,20,0)", type=str, help = "Color: 3 values, comma separated. E.g.: 20,0,0")
 parser.add_argument("-o", "--offset", default=0, type=float, help = "Offset: Scales result somewhat. Best from 0 and 1")
 
 args = parser.parse_args()
 
 color = args.color
 color = color.strip()
-color = color.replace("(","").replace(")","")
 color = color.replace(" ","")
 color = color.split(",")
 r = float(color[0])
