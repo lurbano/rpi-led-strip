4da8267e2387d5b8520c47b7314c0fd70b3c8818
lurbano <lurbano@earthsciweb.org>
2021-06-18 18:10:00 -0500

    intermediate

---
+++ exercises/graphs/pts-dist-1.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+f = plt.figure()
+f.set_figwidth(5)
+f.set_figheight(5)
+
+x = [2,6]
+y = [4,7]
+
+# for i in range(21):
+#     x.append(i)
+#     y.append(i*0.25)
+
+#plt.plot(x,y, 'o', color='black')
+plt.scatter(x,y, edgecolor='black')
+plt.xlabel('x')
+plt.ylabel('y')
+xint = range(0, 11)
+yint = range(0, 11)
+plt.xlim(xmin=0, xmax=10)
+plt.ylim(ymin=0, ymax=10)
+plt.xticks(xint)
+plt.yticks(yint)
+plt.minorticks_on()
+plt.grid(True, which="major", axis="y")
+plt.grid(True, which="major", axis="x")
+
+ax = plt.gca()
+ax.set_axisbelow(True)
+
+f.savefig("pts.svg")
+plt.show()
---
+++ exercises/py/slope.py
@@ -0,0 +1,15 @@
+
+# First Point
+x1 = 2
+y1 = 4
+
+# Second Point
+x2 = 6
+y2 = 7
+
+# Vertical and Horizontal differences
+Dx = x2 - x1
+Dy = y2 - y1
+
+# Calculate Slo
+m = Dy / Dx
