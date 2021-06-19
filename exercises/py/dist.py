72c473b79772b374bf91c89eee7d7fd8b406ee45
lurbano <lurbano@earthsciweb.org>
2021-06-19 18:42:15 -0500

    Points

---
+++ exercises/py/dist.py
@@ -0,0 +1,18 @@
+
+# First Point
+x1 = 2
+y1 = 4
+
+# Second Point
+x2 = 6
+y2 = 7
+
+# Vertical and Horizontal differences
+Dx = x2 - x1
+Dy = y2 - y1
+
+# Calculate Distance
+d = (Dx**2 + Dy**2)**0.5
+
+# Print result
+print(f'The distance is {d}')
